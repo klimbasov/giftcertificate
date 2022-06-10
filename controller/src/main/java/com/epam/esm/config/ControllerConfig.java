fd3b0d4839e2045d923691269b716fdf2a5c45c2
klimbasov <klimbasov.al@gmail.com>
2022-06-10 08:53:10 +0300

    Gradle dependencies optimized.

--- controller/src/main/java/com/epam/esm/config/ControllerConfig.java
+++ controller/src/main/java/com/epam/esm/config/ControllerConfig.java
@@ -3,9 +3,12 @@ package com.epam.esm.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 
@@ -26,5 +29,17 @@ public class ControllerConfig implements WebMvcConfigurer {
         messageResource.setFallbackToSystemLocale(false);
 
         return messageResource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        AcceptHeaderLocaleResolver resolver = new AcceptHeaderLocaleResolver();
+        Locale ruLocale = (new Locale.Builder())
+                .setLanguage("ru")
+                .setRegion("Ru")
+                .build();
+        resolver.setSupportedLocales(Arrays.asList(ruLocale));
+        resolver.setDefaultLocale(Locale.US);
+        return resolver;
     }
 }
